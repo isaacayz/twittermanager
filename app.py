9c05e0369bca02a6fdf522860d7595c0f53cf4e0
Isaac Ige <heywhydot16@gmail.com>
2020-04-12 09:56:14 +0100

    moved keys to a gitignored file

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import tweepy
+import time
+import keys as key
 
-CONSUMER_KEY = 'KnPYfM8bVxZnlouSqTVSdEPwh'
-CONSUMER_SECRET = 'WfggoT2NtGAD56DDPoGubvCVWt2txDHWLjy7cwbgGf6wN7Pori'
-ACCESS_KEY = '384760732-BHwopT7JoZ9AUXcBoy5C2aJtT5ZSSlNqUizQxf9O'
-ACCESS_SECRET = 'mfMn84xBbmkkeeS465SOphLWVm2RGHGq55sYEOKCv0Y2C'
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+access_secret = key.ACCESS_SECRET
+consumer_key = key.CONSUMER_KEY
+consumer_secret = key.CONSUMER_SECRET
+access_key = key.ACCESS_KEY
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
 FILE_NAME = 'last_seen_id.txt'
@@ -23,12 +26,18 @@ def store_last_seen_id(last_seen_id, file_name):
     f_write.close()
     return
 
-last_seen_id = retrieve_last_seen_id(FILE_NAME)
-mentions = api.mentions_timeline(last_seen_id, tweet_mode='extended') 
+def reply_to_tweet():
+    print("Replying to tweet...")
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(last_seen_id, tweet_mode='extended') 
 
-for mention in reversed(mentions):
-    print(str(mention.id) +' _ ' + mention.full_text)
-    last_seen_id = mention.id
-    store_last_seen_id(last_seen_id, FILE_NAME)
-    if '#isaac' in mention.full_text.lower():
-        api.update_status('@' + mention.user.screen_name +' You are doing well small girl. Beep where I am tweeting from (｡♥‿♥｡)', mention.id)
+    for mention in reversed(mentions):
+        print(str(mention.id) +' _ ' + mention.full_text)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        #if '#isaac' in mention.full_text.lower():
+        api.update_status('@' + mention.user.screen_name +' You have reached daddy Isaac, and has it is I am currently unavailable. So, drop your message and I will surely get back to you. You are doing well. Beep where I am tweeting from (｡♥‿♥｡)', mention.id)
+
+while True:
+    reply_to_tweet()
+    time.sleep(15)
